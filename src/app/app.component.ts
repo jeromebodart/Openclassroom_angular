f770dab976b265a3a85c5768d40b448fbd59d17b
jeromebodart <jerome.bodart@hei.yncrea.fr>
2019-01-30 12:14:23 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false;
+    lastUpdate = new Promise((resolve, reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 2000
+      );
+    });
+    appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  /*appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+*/
+  constructor(){
+  	setTimeout(
+  		() => {
+  			this.isAuth = true;
+  		}, 4000
+  	);
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
